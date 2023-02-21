126f64de378f730187a9f75cf49c2935a4c4352c
Andrea Fontana <trikko@users.noreply.github.com>
2023-02-21 18:16:15 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -ti -v /tmp/deploy:/deploy ldc-build /bin/bash -c 'mkdir -p /deploy && tar -czf /deploy/ldc-$LDC_VERSION-armbian.tar.gz /ldc-$LDC_VERSION/*'
+docker run --rm -ti -v /tmp/deploy:/deploy ldc-build /bin/bash -c 'mkdir -p /deploy && tar -czf /deploy/ldc-$LDC_VERSION-arm-bullseye.tar.gz /ldc-$LDC_VERSION/*'
